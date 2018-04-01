519e57e84a78bb6b5400d480edf05cad991c56fd
ahoti <albionahh@gmail.com>
2018-04-01 14:59:54 +0200

    Added changes

--- src/components/layout/NavMenu.js
+++ src/components/layout/NavMenu.js
@@ -8,7 +8,7 @@ export default props => (
   <Navbar inverse fixedTop fluid collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to={'/'}>Ferpoli_jsx</Link>
+        <Link to={'/'}>React Admin Dashboard</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
