c04335832674f28395f680fb826beec8d6dacfd6
hoti <albiona@instaffo.de>
2018-08-02 20:45:38 +0200

    Add changes

--- node_modules/@storybook/ui/src/modules/ui/containers/layout.js
+++
@@ -1,9 +0,0 @@
-import pick from 'lodash.pick';
-import Layout from '../components/layout';
-import genPoddaLoader from '../libs/gen_podda_loader';
-import compose from '../../../compose';
-
-export const mapper = ({ shortcutOptions }) =>
-  pick(shortcutOptions, 'showStoriesPanel', 'showAddonPanel', 'goFullScreen', 'addonPanelInRight');
-
-export default compose(genPoddaLoader(mapper))(Layout);
