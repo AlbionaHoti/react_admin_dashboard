d8aeb6bcae8e7b0bb293c77b8bb812c47185c69d
Albiona <albionahh@outlook.com>
2019-04-02 12:59:31 +0200

    Updates

---
+++ precache-manifest.171a5129442000ce2eab736f4df70f47.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "93fb667c03017a4a3c1b",
+    "url": "/react_admin_dashboard/static/css/main.7aa34e21.chunk.css"
+  },
+  {
+    "revision": "93fb667c03017a4a3c1b",
+    "url": "/react_admin_dashboard/static/js/main.8af0433a.chunk.js"
+  },
+  {
+    "revision": "635a2605b8a8d4e7b4ae",
+    "url": "/react_admin_dashboard/static/js/runtime~main.cbf81373.js"
+  },
+  {
+    "revision": "71fa533f3ade921d4df0",
+    "url": "/react_admin_dashboard/static/css/2.23c42b73.chunk.css"
+  },
+  {
+    "revision": "71fa533f3ade921d4df0",
+    "url": "/react_admin_dashboard/static/js/2.35ebc17e.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react_admin_dashboard/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react_admin_dashboard/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react_admin_dashboard/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react_admin_dashboard/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react_admin_dashboard/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/react_admin_dashboard/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/react_admin_dashboard/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/react_admin_dashboard/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/react_admin_dashboard/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/react_admin_dashboard/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/react_admin_dashboard/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/react_admin_dashboard/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/react_admin_dashboard/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/react_admin_dashboard/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/react_admin_dashboard/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/react_admin_dashboard/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/react_admin_dashboard/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/react_admin_dashboard/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/react_admin_dashboard/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/react_admin_dashboard/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/react_admin_dashboard/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a00c59f4d22f96639ac2e4f22a5fdcfe",
+    "url": "/react_admin_dashboard/index.html"
+  }
+];
